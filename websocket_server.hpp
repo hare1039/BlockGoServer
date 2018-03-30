1383b76cf567c0e7cef4731bc9c7605c7594d53b
hare1039 <hare39393@gmail.com>
2018-03-30 11:44:38 +0800

    Add game attribute to reset pos

--- websocket_server.hpp
+++ websocket_server.hpp
@@ -33,10 +33,13 @@ public:
 	{
 		spdlog::stdout_color_mt("websocket");
 		server.init_asio();
+
 		server.set_message_handler ([this](websocketpp::connection_hdl hdl, decltype(server)::message_ptr msg){this->on_message(hdl, msg);});
 		server.set_open_handler    ([this](websocketpp::connection_hdl hdl){this->on_open(hdl);});
 		server.set_fail_handler    ([this](websocketpp::connection_hdl hdl){this->on_fail(hdl);});
 		server.set_close_handler   ([this](websocketpp::connection_hdl hdl){this->on_close(hdl);});
+
+		server.set_access_channels(websocketpp::log::alevel::none);
 	}
 	void run(int on_port)
 	{
@@ -52,7 +55,11 @@ private:
 
 	std::map<websocketpp::connection_hdl,
 	         std::unique_ptr<std::thread>,
-	         std::owner_less<websocketpp::connection_hdl>> thread_pool;
+	         std::owner_less<websocketpp::connection_hdl>> game_pool;
+
+	std::map<websocketpp::connection_hdl,
+             nlohmann::json,
+             std::owner_less<websocketpp::connection_hdl>> game_attrbute;
 
 	void on_open  (websocketpp::connection_hdl hdl)
 	{
@@ -60,31 +67,34 @@ private:
 		auto io = std::make_unique<boost::asio::io_service>();
 		spdlog::get("websocket")->trace("game.emplace");
 		game.emplace(hdl, new blockgo::game_ctrl(*this, hdl, *io));
-		spdlog::get("websocket")->trace("thread_pool.emplace");
-		thread_pool.emplace(hdl, new std::thread{[io = std::move(io)]{io->run();}});
+		spdlog::get("websocket")->trace("game_pool.emplace");
+		game_pool.emplace(hdl, new std::thread{[io = std::move(io)]{io->run();}});
+		game_attrbute[hdl] = nlohmann::json{};
 	}
 	void on_fail  (websocketpp::connection_hdl hdl)
 	{
 		spdlog::get("websocket")->error("Connection failed: {}", hdl.lock().get());
         game.erase(hdl);
         spdlog::get("websocket")->error("joining thread: {}", hdl.lock().get());
-        thread_pool[hdl]->join();
-        thread_pool.erase(hdl);
+        game_pool[hdl]->join();
+        game_pool.erase(hdl);
+        game_attrbute.erase(hdl);
 	}
 	void on_close (websocketpp::connection_hdl hdl)
 	{
 		spdlog::get("websocket")->info("Closing connection: {}", hdl.lock().get());
         game.erase(hdl);
         spdlog::get("websocket")->trace("joining thread: {}", hdl.lock().get());
-        thread_pool[hdl]->join();
-        thread_pool.erase(hdl);
+        game_pool[hdl]->join();
+        game_pool.erase(hdl);
+        game_attrbute.erase(hdl);
 	}
 	void on_message (websocketpp::connection_hdl hdl, decltype(server)::message_ptr msg)
 	{
 		try
 		{
 			auto command = nlohmann::json::parse(msg->get_payload());
-			ask_block_go(*game[hdl], command);
+			ask_block_go(hdl, command);
 		}
 		catch (websocketpp::lib::error_code const &e)
 		{
@@ -99,6 +109,8 @@ private:
 	void send(websocketpp::connection_hdl const &hdl, std::string s) override
 	{
 		spdlog::get("websocket")->trace("sending by hdl: {}", s);
+
+		// special strings
 		if (s == "")
 		{
 			server.send(hdl, (nlohmann::json{
@@ -107,38 +119,67 @@ private:
 				{"why", ""}
 			}).dump(), websocketpp::frame::opcode::text);
 		}
+		else if (s == "P1 win" || s == "P2 win" || s == "DRAW")
+		{
+			server.send(hdl, (nlohmann::json{
+				{"cmd", "status"},
+				{"status", "end"},
+				{"why", s}
+			}).dump(), websocketpp::frame::opcode::text);
+			return;
+		}
+
+
 		try
 		{
 			auto res = nlohmann::json::parse(s);
 			res["cmd"]    = "transfer";
 			res["x"]      = res["x"].get<int>() - 1;
 			res["y"]      = res["y"].get<int>() - 1;
-			res["rotate"] = 4 - (res["rotate"].get<int>() % 4);
+			res["rotate"] = (res["rotate"].get<int>() + 3) % 4;
 			server.send(hdl, res.dump(), websocketpp::frame::opcode::text);
 		}
 		catch (nlohmann::json::parse_error const &e)
 		{
-			spdlog::get("websocket")->error("process output json failed because: {}", e.what());
 			server.send(hdl, (nlohmann::json{
 				{"cmd", "status"},
 				{"status", "err"},
-				{"why", "unable to parse returned json"}
+				{"why", s},
+				{"origin", game_attrbute[hdl]}
 			}).dump(), websocketpp::frame::opcode::text);
+
+			// try revert to (1, 1)
+			if (game_attrbute[hdl].count("stone") != 0)
+			{
+				spdlog::get("websocket")->debug("trying to revert map");
+				int x      = game_attrbute[hdl]["x"];
+				int y      = game_attrbute[hdl]["y"];
+				int rotate = game_attrbute[hdl]["rotate"];
+				std::stringstream command;
+				std::fill_n(std::ostream_iterator<char>(command), 4 - rotate, 'c');
+				std::fill_n(std::ostream_iterator<char>(command), x, 'a');
+				std::fill_n(std::ostream_iterator<char>(command), y, 'w');
+				game[hdl]->send_stdin(command.str());
+				game_attrbute[hdl] = nlohmann::json{};
+			}
 		}
 		catch (nlohmann::json::exception const &e)
 		{
+			using namespace std::literals;
 			spdlog::get("websocket")->error("json error: {}", e.what());
             server.send(hdl, (nlohmann::json{
                 {"cmd", "status"},
                 {"status", "err"},
-                {"why", "unknown json error"}
+                {"why", "unexpect json error: "s + e.what()}
             }).dump(), websocketpp::frame::opcode::text);
 		}
 		spdlog::get("websocket")->trace("sent: {}", s);
 	}
 
-	void ask_block_go(blockgo::game_ctrl &blockgo, nlohmann::json const & json)
+	void ask_block_go(websocketpp::connection_hdl const &hdl, nlohmann::json const & json)
 	{
+		blockgo::game_ctrl &blockgo = *game[hdl];
+		game_attrbute[hdl] = json;
 		spdlog::get("websocket")->debug("{}", json.dump(4));
 		try
 		{
@@ -146,14 +187,14 @@ private:
 			{
 			case "start"_:
 			{
-				if (json["left"] == "MCTS" && json["right"] == "human")
-				{
+				if      (json["left"] == "MCTS"  && json["right"] == "human")
 					blockgo.send_stdin("4");
-				}
 				else if (json["left"] == "human" && json["right"] == "MCTS")
-				{
 					blockgo.send_stdin("3");
-				}
+				else if (json["left"] == "MCTS"  && json["right"] == "MCTS")
+					blockgo.send_stdin("2");
+				else if (json["left"] == "human" && json["right"] == "human")
+					blockgo.send_stdin("1");
 				break;
 			}
 			case "transfer"_:
@@ -165,10 +206,11 @@ private:
 				std::stringstream command;
 
 				// converting json commands to fed into BlockGo
+
 				command << type;
+				std::fill_n(std::ostream_iterator<char>(command), rotate, 'c');
 				std::fill_n(std::ostream_iterator<char>(command), x, 'd');
 				std::fill_n(std::ostream_iterator<char>(command), y, 's');
-				std::fill_n(std::ostream_iterator<char>(command), rotate, 'c');
 				command << 'y';
 
 				blockgo.send_stdin(command.str());
